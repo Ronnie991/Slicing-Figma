52391c9f5952c75abdc1432415d4bd49d8406a62
Ronnie991 <93050647+Ronnie991@users.noreply.github.com>
2022-10-06 20:08:12 +0700

    Add files via upload

---
+++ application/views/page/landing.php
@@ -0,0 +1,165 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing Page Borneo Cell</title>
+
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/css/bootstrap.css">
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/css/bootstrap-grid.css">
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/css/style.css">
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/libs/fontawesome/css/fontawesome.css">
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/libs/fontawesome/css/fontawesome.min.css">
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/libs//fontawesome/css/all.css">
+
+</head>
+
+<body class="background-blue">
+    <div>
+        <div class="d-flex flex-column flex-sm-row mx-4 pt-4">
+            <h4 class="font-weight-bold mx-5"> Borneo Cell </h4>
+            <p class="mx-5"> About </p>
+            <p class="mx-5"> Features </p>
+            <p class="mx-5"> Services </p>
+            <p class="mx-5"> Pricing </p>
+            <p class="mx-5"> Contact Us </p>
+            <p class="mx-5"> Login </p>
+            <button type="button" class="btn btn-danger mx-5">Sign Up</button>
+        </div>
+        <div class="text-center mx-4">
+            <br>
+            <br>
+            <br>
+            <br>
+            <h2 class="font-weight-bold">Ayo belanja Di Borneo Cell</h2>
+            <h2 class="font-weight-bold">Menyediakan Berbagai Barang Elektronik<h2>
+                    <br>
+                    <h6 class="container align-self-center text-center">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled
+                        it to make a type specimen book. <h6>
+        </div>
+        <div>
+            <br>
+            <img src="<?= base_url(); ?>assets/asset/Img 1.png" class="img-fluid mx-auto d-block" alt="">
+        </div>
+        <div>
+            <br>
+            <br>
+            <h3 class="text-center font-weight-bold">Layanan Yang Kami Sediakan</h3>
+        </div>
+        <div class="container align-content-center">
+            <div class="row mt-3">
+                <div class="mb-1 col-sm-6 col-md-3">
+                    <div class="media  px-2 py-3">
+                        <img class="py-2 px-2 " src="<?= base_url(); ?>assets/asset/VectorPeople.svg" alt="Generic placeholder image">
+                        <div>
+                            <b class="mt-0 text-black">Services </b>
+                            <p class="text-align-justify">Penjualan Smartphone, Aksesoris, Service Smartphone, dan lainnya.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="mb-1 col-sm-6 col-md-3">
+                    <div class="media   border-white px-2 py-3">
+                        <img class="py-2 px-2 align-self-center mr-3" src="<?= base_url(); ?>assets/asset/VectorPaper.svg" alt="Generic placeholder image">
+                        <div>
+                            <b class="mt-0 text-black">Services 2 </b>
+                            <p class="text-align-justify">Isi Pulsa, Paket Kuota
+                                Token Listrik, transfer
+                                uang.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="mb-1 col-sm-6 col-md-3">
+                    <div class="media border-white px-2 py-3">
+                        <img class="py-2 px-2 align-self-center mr-3" src="<?= base_url(); ?>assets/asset/VectorTime.svg" alt="Generic placeholder image">
+                        <div>
+                            <b class="mt-0 text-black">Operational Time</b>
+                            <p class="text-align-justify">Toko kami buka jam 10.00
+                                Sampai dengan 21.30 WIB</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="container align-content-left">
+                    <div class="row mt-3">
+                        <div class="mb-1 col-sm-6 col-md-6">
+                            <img class="py-2 px-2" src="<?= base_url(); ?>assets/asset/Img 2.png" alt="Generic placeholder image">
+                        </div>
+                        <div class="mb-2 col-sm-6 col-md-6">
+                            <div class="px-2 py-3">
+                                <div>
+                                    <br>
+                                    <br>
+                                    <br>
+                                    <br>
+                                    <br>
+                                    <br>
+                                    <br>
+                                    <br>
+                                    <br>
+                                    <br>
+                                    <br>
+                                    <p class="mt-0 font-weight-bold text-center">HP OPPO merupakan salah satu merk smartphone paling
+                                        populer di Asia, termasuk Indonesia. Merk smartphone asal China ini rutin
+                                        meluncurkan HP OPPO terbaru setiap tahunnya. </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="container background-darkblue">
+                        <div class="row">
+                            <div class="col-2">
+                                <p class="font-weight-bold text-white">About</p>
+                                <br>
+                                <p class="text-white"> About</p>
+                                <p class="text-white"> Services</p>
+                                <p class="text-white"> Features</p>
+                                <p class="text-white"> Term</p>
+                            </div>
+                            <div class="col-2">
+                                <p class="font-weight-bold text-white">Portofolio</p>
+                                <br>
+                                <p class="text-white"> Web Apps</p>
+                                <p class="text-white"> EWebsites</p>
+                                <p class="text-white"> iOS App</p>
+                                <p class="text-white"> Illustration</p>
+                            </div>
+                            <div class="col-2">
+                                <p class="font-weight-bold text-white">Consultancy</p>
+                                <br>
+                                <p class="text-white"> Marketing</p>
+                                <p class="text-white"> Social Media</p>
+                                <p class="text-white"> Management</p>
+                                <p class="text-white"> Custom Plans</p>
+                            </div>
+                            <div class="col-2">
+                                <p class="font-weight-bold text-white">Contact Us</p>
+                                <br>
+                                <p class="text-white"> Contacs</p>
+                                <p class="text-white"> Team Members</p>
+                                <p class="text-white"> History</p>
+                                <p class="text-white"> anonymous@gmail.com</p>
+                            </div>
+
+                            <div class="col">
+                                <b class="text-white">Official Account</b><br><br>
+                                <div class="d-inline py-2 px-2  rounded"><i class="fab fa-fw fa-1x fa-facebook text-white"></i></div>
+                                <div class="d-inline py-2 px-2  rounded"><i class="fab fa-fw fa-1x fa-twitter text-white"></i></div>
+                                <div class="d-inline py-2 px-2  rounded"><i class="fab fa-fw fa-1x fa-line text-white"></i></div>
+                                <div class="d-inline py-2 px-2   rounded"><i class="fab fa-fw fa-1x fa-whatsapp text-white"></i></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <script src="js/bootstrap.js"></script>
+
+</body>
+
+</html>
